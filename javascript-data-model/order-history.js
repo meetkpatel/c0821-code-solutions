7ddb5b98b687cbedddc567211799e02d529b9cd8
Meet Patel <mr.meetkpatel@gmail.com>
2021-08-24 12:52:57 -0700

    Changes done to javascript-data-model exercise.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,44 +1,61 @@
 var orderHistory = [
   {
     orderNo: '114-39416898772232',
-    productName: 'JavaScript for impatient programmers',
-    bookAuthor: 'Rauschmayer, Dr.Axel',
     shipTo: 'JS Masher',
-    price: '$31.55',
     total: '$34.00',
     orderPlaced: 'August 4, 2020',
     orderDelivered: 'August 8, 2020',
-    returnWindow: 'Sep 7, 2020'
+    items: [{
+      productName: 'JavaScript for impatient programmers',
+      bookAuthor: 'Rauschmayer, Dr.Axel',
+      price: '$31.55',
+      returnWindow: 'Sep 7, 2020'
+    }]
   },
   {
     orderNo: '113-9984268-1280257',
-    productName: 'The Timeless Way of Building',
-    bookAuthor: 'Alexander, Christopher',
     shipTo: 'JS Masher',
-    price: '$41.33',
-    total: '$34.53',
+    total: '$44.53',
     orderPlaced: 'July 19, 2020',
     orderDelivered: 'July 20, 2020',
-    returnWindow: 'Aug 19, 2020'
+    items: [{
+      productName: 'The Timeless Way of Building',
+      bookAuthor: 'Alexander, Christopher',
+      price: '$41.33',
+      returnWindow: 'Aug 19, 2020'
+    }]
   },
   {
     orderNo: '114-2875557-9059409',
-    productName: 'Gamecube Controller Adapter',
     shipTo: 'JS Masher',
-    price: '$15.98',
     total: '$17.22',
     orderPlaced: 'July 4, 2020',
     orderDelivered: 'July 7, 2020',
-    returnWindow: 'Aug 5, 2020'
-  }, {
+    items: [{
+      productName: 'Gamecube Controller Adapter',
+      bookAuthor: null,
+      price: '$15.98',
+      returnWindow: 'Aug 5, 2020'
+    }]
+  },
+  {
     orderNo: '113-2883177-2648248',
-    productName: ['Gamecube Controller', 'The Art of Sql'],
-    bookAuthor: [null, 'Farouly, Stephane'],
     shipTo: 'JS Masher',
-    price: ['$94.95', '$33.99'],
     total: '$138.93',
     orderPlaced: 'July 3, 2020',
     orderDelivered: 'July 5, 2020',
-    returnWindow: 'Aug 4, 2020'
+    items: [{
+      productName: 'Gamecube Controller',
+      bookAuthor: null,
+      price: '$94.95',
+      returnWindow: 'Aug 5, 2020'
+    },
+    {
+      productName: 'The Art of Sql',
+      bookAuthor: 'Farouly, Stephane',
+      price: '$33.99',
+      returnWindow: 'Aug 5, 2020'
+    }
+    ]
   }
 ];
