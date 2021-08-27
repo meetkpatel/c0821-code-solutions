8e918442f76c17986cae101e66c9286d7d4d8d9e
Meet Patel <mr.meetkpatel@gmail.com>
2021-08-27 09:38:57 -0700

    removed nasted loop in student name/javascript-loops-practice exercise.

--- javascript-loops-practice/get-student-names.js
+++ javascript-loops-practice/get-student-names.js
@@ -2,10 +2,7 @@
 function getStudentNames(students) {
   var name = [];
   for (var key in students) {
-    var students2 = students[key];
-    for (var key2 in students2) {
-      name.push(students2[key2]);
-    }
+    name.push(students[key].name);
   }
   return name;
 }
