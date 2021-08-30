07f4e82487c0f928ec428d0cd697f8eb86fedf7a
Meet Patel <mr.meetkpatel@gmail.com>
2021-08-30 13:22:54 -0700

    Changes donw to array manipulation 1  exercise.

--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -1,15 +1,12 @@
 /* exported tail */
 function tail(array) {
   var tailArray = [];
-  for (var i = 0; i < array.length; i++) {
-    if (i > 0) {
-      tailArray.push(array[i]);
-    }
+  for (var i = 1; i < array.length; i++) {
+    tailArray.push(array[i]);
   }
   return tailArray;
 }
 // create storage for empty array
-// get length of list and go throw each list
-// check current index is more then 0
-//    if yes add current list element to storage list element
+// get length of list and go throw each list from index 1
+// add current list element to storage list element
 // return storage list
