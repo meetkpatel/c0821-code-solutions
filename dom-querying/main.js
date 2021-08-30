f50230fb8f5090fdfc4ac0452941576e99eea869
Meet Patel <mr.meetkpatel@gmail.com>
2021-08-30 11:27:35 -0700

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('hello, world');
+var h1Tag = document.querySelector('h1');
+console.log('H1: ', h1Tag);
+console.dir(h1Tag);
+
+var explanationClass = document.querySelector('#explanation');
+console.log('explanation ID: ', explanationClass);
+console.dir(explanationClass);
+
+var hintId = document.querySelector('.hint');
+console.log('hint Class: ', hintId);
+console.dir(hintId);
+
+var nordP = document.querySelectorAll('p');
+console.log(nordP);
+
+var nordClass = document.querySelectorAll('.example-link');
+console.log(nordClass);
