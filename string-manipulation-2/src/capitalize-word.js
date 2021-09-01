bcca7007ec698b5f98575c735a029658fb57b61e
Meet Patel <mr.meetkpatel@gmail.com>
2021-09-01 13:51:13 -0700

    Changes done on string-manipulation-2 exercise.

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,12 +1,9 @@
 /* exported capitalizeWord */
 function capitalizeWord(word) {
   var newString = '';
-  for (var i = 0; i < word.length; i++) {
-    if (i === 0) {
-      newString += word[i].toUpperCase();
-    } else {
-      newString += word[i].toLowerCase();
-    }
+  newString += word[0].toUpperCase();
+  for (var i = 1; i < word.length; i++) {
+    newString += word[i].toLowerCase();
   }
   if (newString === 'Javascript') {
     newString = 'JavaScript';
@@ -14,10 +11,9 @@ function capitalizeWord(word) {
   return newString;
 }
 // create storage for string
+// add and convert first character tp uppercase and store ot to the storage
 // get the length go through each character till the length
-// check if charater is a first charater
-//      if yes change it to capital case and add it to the string storage variable
-// else change it to lower case string storage variable
+// change it to lower case string storage variable
 // check if string is 'Javascript'
 //      if yes change storage string to 'JavaScript'
 // return string storage variable
