d0be9d7a6775091dbec98f369829bcf06fb2034d
Meet Patel <mr.meetkpatel@gmail.com>
2021-09-02 16:58:50 -0700

    Completed array-manipulation-3 exercise.

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,20 @@
 /* exported union */
+function union(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    newArray.push(first[i]);
+  }
+  for (var j = 0; j < second.length; j++) {
+    if (newArray.indexOf(second[j]) === -1) {
+      newArray.push(second[j]);
+    }
+  }
+  return newArray;
+}
+//  create storage list
+//  go through each element of the first array
+//  add current element of first array to the created list
+//  go through each element of the second array
+//  check if indec current element of second array in the first array and check if its === -1
+//        if yes add current element of second array to the created list
+// return created list
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,16 @@
 /* exported unique */
+function unique(array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (newArray.indexOf(array[i]) === -1) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+
+// create a storage for list
+// loop through each array
+// find the index current element in new created list and check if its -1
+//      if yes add the current element to the new storge
+// return new array list
