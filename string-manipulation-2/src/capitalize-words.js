ba91bdde179461cf7ed8b5ab54064506ea75c3e2
Meet Patel <mr.meetkpatel@gmail.com>
2021-09-01 17:08:40 -0700

    Changes done to capitalize words in string-manipulation-2 exercise.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,10 +1,9 @@
 /* exported capitalizeWords */
 function capitalizeWords(string) {
   var newString = '';
-  for (var i = 0; i < string.length; i++) {
-    if (i === 0) {
-      newString += string[i].toUpperCase();
-    } else if (string[i - 1] === ' ') {
+  newString += string[0].toUpperCase();
+  for (var i = 1; i < string.length; i++) {
+    if (string[i - 1] === ' ') {
       newString += string[i].toUpperCase();
     } else {
       newString += string[i].toLowerCase();
@@ -14,10 +13,9 @@ function capitalizeWords(string) {
 }
 
 // create storage for string
+// add and convert first character of string to upperCase and store it to storage
 // get the length go through each character till the length
-// check if its first character
-//      if yes convert current character to into upper case and store it to the storage string variable
-// if not check if its previous charater was space' '
+// check if its previous charater was space' '
 //      if yes convert current character to into upper case and store it to the storage string variable
 // if not change convert current character to into lower case and store it to the storage string variable
 // return storage string variable
