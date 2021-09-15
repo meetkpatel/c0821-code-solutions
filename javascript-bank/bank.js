1c1147932f0c3a6aa394b37d63c7c6feda2e4478
Meet Patel <mr.meetkpatel@gmail.com>
2021-09-15 13:08:04 -0700

    completed javascript-bank exercise.

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -4,13 +4,31 @@ function Bank() {
   this.accounts = [];
 }
 
-// Bank.prototype.openAccount = function (holder, balance) {
-//   if (Number.isInteger(balance) && balance > 0) {
-//     var newAcount = new Account(this.nextAccountNumber, holder);
-//     var depositBalance = new Transaction('deposit', balance);
-//     this.transactions.push(depositTrans);
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
+Bank.prototype.openAccount = function (holder, balance) {
+  if (Number.isInteger(balance) && balance > 0) {
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    this.accounts.push(newAccount);
+    newAccount.deposit(balance);
+    this.nextAccountNumber++;
+    return this.nextAccountNumber - 1;
+  } else {
+    return null;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var bankTotalAssets = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    bankTotalAssets += this.accounts[i].getBalance();
+  }
+  return bankTotalAssets;
+};
