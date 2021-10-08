8b732f5f86006b066f3cd82da81ddaef310120ce
Meet Patel <mr.meetkpatel@gmail.com>
2021-10-08 23:37:38 +0000

    Completed express-delete

---
+++ express-delete/index.js
@@ -0,0 +1,41 @@
+
+var express = require('express');
+const app = express();
+var grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', function (req, res) {
+  var gradesArr = [];
+  for (const i in grades) {
+    gradesArr.push(grades[i]);
+  }
+  res.json(gradesArr);
+});
+
+app.delete('/api/grades/:id', function (req, res) {
+  delete grades[req.params.id];
+  res.sendStatus(204);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
