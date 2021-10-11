97290a5dde86b1d2ef53e2a519f30f8b3425ea7b
Meet Patel <mr.meetkpatel@gmail.com>
2021-10-11 04:59:35 +0000

    changes done.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 var jsonExpress = express.json();
 app.use(jsonExpress);
+var dataFetch = require('./data.json');
+const fs = require('fs');
 
 app.get('/api/notes', function (req, res) {
-  var dataFetch = require('./data.json');
   const noteArray = [];
   for (const i in dataFetch.notes) {
     noteArray.push(dataFetch.notes[i]);
@@ -13,7 +14,6 @@ app.get('/api/notes', function (req, res) {
 });
 
 app.get('/api/notes/:id', function (req, res) {
-  var dataFetch = require('./data.json');
   if (req.params.id < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (dataFetch.notes[req.params.id] === undefined) {
@@ -32,28 +32,31 @@ app.post('/api/notes', function (req, res) {
     reqBody.id = dataForPost.nextId;
     dataForPost.notes[dataForPost.nextId] = reqBody;
     dataForPost.nextId++;
-    const fs = require('fs');
     const dataStr = JSON.stringify(dataForPost, null, 2);
     fs.writeFile('data.json', dataStr, err => {
-      if (err) throw res.status(500).json({ error: 'An unexpected error occurred.' });
-      res.status(201).json(dataForPost.notes[dataForPost.nextId - 1]);
+      if (err) {
+        return res.status(500).json({ error: 'An unexpected error occurred.' });
+      } else {
+        res.status(201).json(dataForPost.notes[dataForPost.nextId - 1]);
+      }
     });
   }
 });
 
 app.delete('/api/notes/:id', function (req, res) {
-  var dataFetchForDelete = require('./data.json');
   if (req.params.id < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
-  } else if (dataFetchForDelete.notes[req.params.id] === undefined) {
+  } else if (dataFetch.notes[req.params.id] === undefined) {
     res.status(404).json({ error: 'cannot find note with id ' + req.params.id });
   } else {
-    delete dataFetchForDelete.notes[req.params.id];
-    const fs = require('fs');
-    const dataStrForDelete = JSON.stringify(dataFetchForDelete, null, 2);
+    delete dataFetch.notes[req.params.id];
+    const dataStrForDelete = JSON.stringify(dataFetch, null, 2);
     fs.writeFile('data.json', dataStrForDelete, err => {
-      if (err) throw res.status(500).json({ error: 'An unexpected error occurred.' });
-      res.status(204).json();
+      if (err) {
+        return res.status(500).json({ error: 'An unexpected error occurred.' });
+      } else {
+        res.status(204).json();
+      }
     });
   }
 });
@@ -61,20 +64,21 @@ app.delete('/api/notes/:id', function (req, res) {
 app.put('/api/notes/:id', function (req, res) {
   var reqBodyPut = req.body;
   reqBodyPut.id = parseInt(req.params.id);
-  var dataFetchForPut = require('./data.json');
   if (req.params.id < 0) {
     res.status(400).json({ error: 'id must be a positive integer' });
   } else if (reqBodyPut.content === undefined) {
     res.status(400).json({ error: 'content is a required field' });
-  } else if (dataFetchForPut.notes[req.params.id] === undefined) {
+  } else if (dataFetch.notes[req.params.id] === undefined) {
     res.status(404).json({ error: 'cannot find note with id ' + req.params.id });
   } else {
-    dataFetchForPut.notes[req.params.id] = reqBodyPut;
-    const fs = require('fs');
-    const dataStrForPut = JSON.stringify(dataFetchForPut, null, 2);
+    dataFetch.notes[req.params.id] = reqBodyPut;
+    const dataStrForPut = JSON.stringify(dataFetch, null, 2);
     fs.writeFile('data.json', dataStrForPut, err => {
-      if (err) throw res.status(500).json({ error: 'An unexpected error occurred.' });
-      res.status(200).json(dataFetchForPut.notes[req.params.id]);
+      if (err) {
+        return res.status(500).json({ error: 'An unexpected error occurred.' });
+      } else {
+        res.status(200).json(dataFetch.notes[req.params.id]);
+      }
     });
   }
 });
